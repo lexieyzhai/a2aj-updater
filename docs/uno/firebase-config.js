92b99322ac36d39cf6c218e49e0af51c122c9f44
mac <mac@macdeMacBook-Pro-7.local>
2026-04-16 10:36:48 -0400

    Configure Firebase for UNO

--- docs/uno/firebase-config.js
+++ docs/uno/firebase-config.js
@@ -1,12 +1,12 @@
 window.LITERALLY_UNO_FIREBASE = {
-  enabled: false,
+  enabled: true,
   config: {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: ""
+    apiKey: "AIzaSyCH_M88L1L7q_xvwIL8pYthHJbvaS07Aic",
+    authDomain: "uno-d0148.firebaseapp.com",
+    databaseURL: "https://uno-d0148-default-rtdb.firebaseio.com",
+    projectId: "uno-d0148",
+    storageBucket: "uno-d0148.firebasestorage.app",
+    messagingSenderId: "6543574998",
+    appId: "1:6543574998:web:a306d3018ee308a465c677"
   }
 };
